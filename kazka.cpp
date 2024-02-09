0eea47a7128734fe4c7fa9607790bbbe9da2639f
paulynaa <147087833+paulynaa@users.noreply.github.com>
2024-02-09 14:28:46 +0200

    Add files via upload

--- kazka.cpp
+++ kazka.cpp
@@ -8,14 +8,16 @@ struct Pazymiai{
     double vid;
     int egz;
     double galutinis;
+    double med;
     };
 
 
 int main ()
 {
+    int c;
     int n;
     int kiek;
-    int paz;
+    int paz[20];
     int suma=0;
     cout<<"Iveskite studentu skaiciu   "<<endl;
     cin>>n;
@@ -31,22 +33,49 @@ int main ()
         for(int j=0; j<kiek; j++)
         {
             cout<<"Iveskite "<<j+1<<" pazymi   "<<endl;
-            cin>>paz;
-            suma=suma+paz;
+            cin>>paz[j];
+            suma=suma+paz[j];
         }
+
+
+
         P[i].vid=suma/kiek;
         cout <<"Iveskite egzamino rezultata   "<<endl;
         cin>>P[i].egz;
         P[i].galutinis=P[i].vid*0.4+0.6*P[i].egz;
+
+
+   for(int k=0; k<kiek-1; k++)
+    {
+        for(int l=k+1; l<kiek; l++)
+       {
+           if(paz[k]>paz[l])
+           {
+               c=paz[k];
+            paz[k]=paz[l];
+            paz[l]=c;
+        }
+        }
     }
 
+    if (kiek%2==0)
+    {
+       int pirmas=kiek/2-1;
+       int antras=kiek/2;
+       P[i].med=(paz[pirmas]+paz[antras])/2.0;
+    }
+    else{
+        int vidurys = kiek / 2;
+            P[i].med = paz[vidurys];
+    }
+    }
     cout<<endl;
 
-    cout<<"Pavarde\t "<< "Vardas\t"<<"Galutinis\t"<<endl;
+    cout<<"Pavarde\t "<< "Vardas\t"<<"Galutinis (Vid) \ Mediana (Vid)"<<endl;
     cout<<"------------------------------"<<endl;
     for(int i=0; i<n; i++)
     {
-        cout<<P[i].pav<<"\t"<<P[i].var<<"\t"<<fixed<<setprecision(2)<<P[i].galutinis<<endl;
+        cout<<P[i].pav<<"\t"<<P[i].var<<"\t"<<fixed<<setprecision(2)<<P[i].galutinis<<"\t"<<fixed<<setprecision(2)<<P[i].med<<endl;
 
     }
 
