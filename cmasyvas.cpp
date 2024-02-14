7d91a6b817f9dc4053ebdd36b6d3637ee379acad
paulynaa <147087833+paulynaa@users.noreply.github.com>
2024-02-14 16:36:32 +0200

    Add files via upload

--- cmasyvas.cpp
+++ cmasyvas.cpp
@@ -33,6 +33,7 @@ Pazymiai C[7];
         case 1:{
 
         do{
+
         double suma=0.0;
 
         cout<<"Iveskite "<<i+1<<" pavarde (noretume baigti spauskite 4):   "<<endl;
@@ -49,6 +50,7 @@ Pazymiai C[7];
         int j=0;
 
             do{
+
             cout<<"Iveskite "<<j+1<<" pazymi (noredami baigti spauskite 11):  "<<endl;
             cin>>C[i].paz[j];
             if(C[i].paz[j]==11){
@@ -59,7 +61,9 @@ Pazymiai C[7];
                 cin >> C[i].paz[j];
             }
             suma=suma+C[i].paz[j];
+
         j++;
+
         }
         while(C[i].paz[j]!=11);
 
@@ -67,13 +71,17 @@ Pazymiai C[7];
 
         cout <<"Iveskite egzamino rezultata: "<<endl;
         cin>>C[i].egz;
-        while (C[i].egz < 1 || C[i].egz > 10) {
-            cout << "Iveskite egzamino rezultata nuo 1 iki 10: ";
-            cin >> C[i].egz;
-        }
-        C[i].galutinis=(C[i].vid*0.4)+(0.6*C[i].egz);
 
-        for(int k=0; k<j-1; k++)
+        while (cin.fail() || C[i].egz < 1 || C[i].egz > 10) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "Iveskite egzamino rezultata nuo 1 iki 10: ";
+                    cin >> C[i].egz;
+                }
+
+        C[i].galutinis=(C[i].vid*0.4)+(0.6*C[i].egz);
+        // sortiravimas sort(C[i].paz.begin(), C[i].paz.end());
+         for(int k=0; k<j-1; k++)
         {
             for(int l=k+1; l<j; l++)
             {
@@ -84,6 +92,7 @@ Pazymiai C[7];
                 }
             }
         }
+
         if (j%2==0)
         {
         int pirmas=j/2-1;
@@ -94,9 +103,13 @@ Pazymiai C[7];
             int vidurys = j / 2;
             C[i].med = C[i].paz[vidurys];
             }
+
             i++;
-        }
-        while(C[i].pav!="4" || C[i].var!="4");
+
+
+        }while(C[i].pav!="4" || C[i].var!="4");
+
+
 
         cout<<left<<setw(15)<<"Pavarde "<<setw(15)<< "Vardas"<<setw(17)<<"Galutinis (Vid.)"<<" / "<<setw(17)<<"Galutinis (Med.)"<<endl;
     cout<<"--------------------------------------------------------"<<endl;
@@ -106,196 +119,263 @@ Pazymiai C[7];
 
     }
         break;
+
         }
 
         case 2:{
-           do {
 
-        double suma=0.0;
 
-        cout<<"Iveskite "<<i+1<<" pavarde (noredami sustoti iveskite 4):   "<<endl;
-        cin>>C[i].pav;
-        if(C[i].pav=="4" || C[i].var=="4"){
+           do  {double suma = 0.0;
+
+
+        cout << "Iveskite " << i + 1 << " pavarde (noredami sustoti iveskite 4):   " << endl;
+        cin >> C[i].pav;
+        if (C[i].pav == "4" || C[i].var == "4") {
             break;
         }
-        cout<<"Iveskite "<<i+1<<" varda (noredami sustoti iveskite 4):   "<<endl;
-        cin>>C[i].var;
-        if(C[i].pav=="4" || C[i].var=="4"){
+
+        cout << "Iveskite " << i + 1 << " varda (noredami sustoti iveskite 4):   " << endl;
+        cin >> C[i].var;
+        if (C[i].pav == "4" || C[i].var == "4") {
             break;
         }
-        cout<<"Jeigu norite generuoti po viena atsitiktini skaiciu spauskite 1 "<<endl<<"Jeigu norite generuoti pasirinkta skaiciu spauskite 2 "<<endl;
-        cout<<"Jusu pasirinkimas: "; cin>>x;
-        if(x==1){
-                string y;
-                int h=0;
-            while(y!="Ne"){
 
+        cout << "Jeigu norite generuoti po viena atsitiktini skaiciu spauskite 1 " << endl
+             << "Jeigu norite generuoti pasirinkta skaiciu spauskite 2 " << endl;
+        cout << "Jusu pasirinkimas: ";
+
+        while (!(cin >> x) || (x != 1 && x != 2)) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Iveskite skaiciu 1 arba 2: ";
+    }
+
+        if (x == 1) {
+            string y;
+            int h = 0;
+            while (y != "Ne") {
                 C[i].paz[h]=(rand()%10)+1;
                 cout<<"Pazymis "<<h+1<<" : "<<C[i].paz[h]<<endl;
                 cout<<"Ar norite testi generavima? Taip arba Ne: ";
                 cin>>y; cout<<endl;
                 suma=suma+C[i].paz[h];
                 h++;
-                }
-               C[i].vid=suma/h;
+            }
+            C[i].vid = suma / h;
 
-                sort(C[i].paz+0, C[i].paz+h);
+            sort(C[i].paz+0, C[i].paz+h);
 
-                if (h%2==0)
-        {
-        int pirmas=h/2-1;
-        int antras=h/2;
-        C[i].med = (C[i].paz[pirmas] + C[i].paz[antras])/2.0;
+            if (h % 2 == 0) {
+                int pirmas = h / 2 - 1;
+                int antras = h / 2;
+                C[i].med = (C[i].paz[pirmas] + C[i].paz[antras]) / 2.0;
+            } else {
+                int vidurys = h / 2;
+                C[i].med = C[i].paz[vidurys];
+            }
         }
-        else{
-            int vidurys = h / 2;
-            C[i].med = C[i].paz[vidurys];
+
+        if (x == 2) {
+            int k;
+            cout << "Iveskite pazymiu kieki: ";
+
+            while (!(cin >> k) || (k < 1)) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Iveskite skaiciu nuo 1: ";
+    }
+            for (int l = 0; l < k; l++) {
+                C[i].paz[l]=(rand() % 10) + 1;
+                cout << "Pazymis " << l + 1 << " : " << C[i].paz[l] << endl;
+                suma = suma + C[i].paz[l];
             }
 
-            }
-
-            if (x==2){
-                    int k;
-                cout<<"Iveskite pazymiu kieki: "; cin>>k;
-            for(int l=0; l<k; l++){
-                C[i].paz[l]=(rand()%10)+1;
-                cout<<"Pazymis "<<l+1<<" : "<<C[i].paz[l]<<endl;
-                suma=suma+C[i].paz[l];
-            }
-            C[i].vid=suma/k;
+            C[i].vid = suma / k;
 
             sort(C[i].paz+0, C[i].paz+k);
 
-            if (k%2==0)
-        {
-        int pirmas=k/2-1;
-        int antras=k/2;
-        C[i].med = (C[i].paz[pirmas] + C[i].paz[antras])/2.0;
+            if (k % 2 == 0) {
+                int pirmas = k / 2 - 1;
+                int antras = k / 2;
+                C[i].med = (C[i].paz[pirmas] + C[i].paz[antras]) / 2.0;
+            } else {
+                int vidurys = k / 2;
+                C[i].med = C[i].paz[vidurys];
+            }
         }
-        else{
-            int vidurys = k / 2;
-            C[i].med = C[i].paz[vidurys];
-            }
-            }
+
         int w;
-        cout<<"Jeigu norite atsitiktinai sugeneruoti egzamino rezultata spauskite 1: "<<endl;
-        cout<<"Jeigu norite ivesti egzamino rezultata spauskite 2: "<<endl;
-        cout<<"Jusu pasirinkimas: "; cin>>w;
-        if(w==1){
-            C[i].egz=(rand()%10)+1;
-            cout<<"Egzamino rezultatas: "<<C[i].egz<<endl;
-        }
-        if(w==2){
-            cout<<"Iveskite egzamino rezultata: ";
-            cin>>C[i].egz;
-            cout<<endl;
+        cout << "Jeigu norite atsitiktinai sugeneruoti egzamino rezultata spauskite 1: " << endl;
+        cout << "Jeigu norite ivesti egzamino rezultata spauskite 2: " << endl;
+        cout << "Jusu pasirinkimas: ";
+
+        while (!(cin >> w) || (w != 1 && w != 2)) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Iveskite skaiciu 1 arba 2: ";
+    }
+
+        if (w == 1) {
+            C[i].egz = (rand() % 10) + 1;
+            cout << "Egzamino rezultatas: " << C[i].egz << endl;
         }
 
-        C[i].galutinis=(C[i].vid*0.4)+(0.6*C[i].egz);
+        if (w == 2) {
+            cout << "Iveskite egzamino rezultata: ";
+
+            while (!(cin >> C[i].egz) || (C[i].egz <1 || C[i].egz >10)) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Iveskite egzamino rezultata nuo 1 iki 10: ";
+    }
+            cout << endl;
+        }
+
+        C[i].galutinis = (C[i].vid * 0.4) + (0.6 * C[i].egz);
+
         i++;
-        }
-        while(C[i].pav!="4" || C[i].var!="4");
+    }
+    while (C[i].pav != "4" || C[i].var != "4");
 
- cout<<left<<setw(15)<<"Pavarde "<<setw(15)<< "Vardas"<<setw(17)<<"Galutinis (Vid.)"<<" / "<<setw(17)<<"Galutinis (Med.)"<<endl;
+    cout<<left<<setw(15)<<"Pavarde "<<setw(15)<< "Vardas"<<setw(17)<<"Galutinis (Vid.)"<<" / "<<setw(17)<<"Galutinis (Med.)"<<endl;
     cout<<"--------------------------------------------------------"<<endl;
     for(int q=0; q<i; q++)
     {
         cout<<left<<setw(15)<<C[q].pav<<setw(15)<<C[q].var<<setw(17)<<fixed<<setprecision(2)<<C[q].galutinis<<setw(17)<<fixed<<setprecision(2)<<C[q].med<<endl;
 
     }
-        break;
+    break;
         }
+
         case 3:{
-            int qq=0;
-        string vardai[] = {"Paulina","Adriana","Gitanas","Donald", "Ugne", "Kamile", "Rugile", "Roberta","Valdemaras","Jurgis"};
-        string  pavardes[] = { "Podgaiska","Obama","Trump","Nauseda","Sirokyte", "Mockute", "Zobelaite", "Macaite", "Jurpalyte","Jankauskas"};
-        while(qq!=1){
+            int qq = 0;
+    string vardai[] = {"Paulina", "Adriana", "Gitanas", "Donald", "Ugne", "Kamile", "Rugile", "Roberta", "Valdemaras", "Jurgis"};
+    string pavardes[] = {"Podgaiska", "Obama", "Trump", "Nauseda", "Sirokyte", "Mockute", "Zobelaite", "Macaite", "Jurpalyte", "Jankauskas"};
 
-            C[i].pav=pavardes[rand()%10];
-            C[i].var=vardai[rand()%10];
-            cout<<"Pavarde ir vardas: "<<C[i].pav<<" "<<C[i].var<<endl;
-        cout<<"Jeigu norite generuoti po viena atsitiktini skaiciu spauskite 1 "<<endl<<"Jeigu norite generuoti pasirinkta skaiciu spauskite 2 "<<endl;
-        cout<<"Jusu pasirinkimas: "; cin>>x;
-        if(x==1){
-                string y;
-                int h=0;
-                double suma=0.0;
-            while(y!="Ne"){
+    while (qq != 1) {
 
-                C[i].paz[h]=(rand()%10)+1;
-                cout<<"Pazymis "<<h+1<<" : "<<C[i].paz[h]<<endl;
-                cout<<"Ar norite testi generavima? Taip arba Ne: ";
-                cin>>y; cout<<endl;
-                suma=suma+C[i].paz[h];
+        C[i].pav = pavardes[rand() % 10];
+        C[i].var = vardai[rand() % 10];
+        cout << "Pavarde ir vardas: " << C[i].pav << " " << C[i].var << endl;
+
+        cout << "Jeigu norite generuoti po viena atsitiktini skaiciu spauskite 1 " << endl
+             << "Jeigu norite generuoti pasirinkta skaiciu spauskite 2 " << endl;
+        cout << "Jusu pasirinkimas: ";
+        while (!(cin >> x) || (x != 1 && x != 2)) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Iveskite skaiciu 1 arba 2: ";
+    }
+
+        if (x == 1) {
+            string y;
+            int h = 0;
+            double suma = 0.0;
+
+            while (y != "Ne") {
+                C[i].paz[h]=(rand() % 10) + 1;
+                cout << "Pazymis " << h + 1 << " : " << C[i].paz[h] << endl;
+                cout << "Ar norite testi generavima? Taip arba Ne: ";
+                cin >> y;
+                cout << endl;
+                suma = suma + C[i].paz[h];
                 h++;
-                }
-               C[i].vid=suma/h;
+            }
+            C[i].vid = suma / h;
 
-                sort(C[i].paz+0, C[i].paz+h);
+            sort(C[i].paz+0, C[i].paz+h);
 
-                if (h%2==0)
-        {
-        int pirmas=h/2-1;
-        int antras=h/2;
-        C[i].med = (C[i].paz[pirmas] + C[i].paz[antras])/2.0;
+            if (h % 2 == 0) {
+                int pirmas = h / 2 - 1;
+                int antras = h / 2;
+                C[i].med = (C[i].paz[pirmas] + C[i].paz[antras]) / 2.0;
+            } else {
+                int vidurys = h / 2;
+                C[i].med = C[i].paz[vidurys];
+            }
         }
-        else{
-            int vidurys = h / 2;
-            C[i].med = C[i].paz[vidurys];
+
+        if (x == 2) {
+            double suma = 0.0;
+            int k;
+            cout << "Iveskite pazymiu kieki: ";
+
+            while (!(cin >> k) || (k < 1 )) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Iveskite skaiciu nuo 1 : ";
+        }
+
+            for (int l = 0; l < k; l++) {
+                C[i].paz[l]=(rand() % 10) + 1;
+                cout << "Pazymis " << l + 1 << " : " << C[i].paz[l] << endl;
+                suma = suma + C[i].paz[l];
             }
-            }
-            if (x==2){
-                    double suma=0.0;
-                    int k;
-                cout<<"Iveskite pazymiu kieki: "; cin>>k;
-            for(int l=0; l<k; l++){
-                C[i].paz[l]=(rand()%10)+1;
-                cout<<"Pazymis "<<l+1<<" : "<<C[i].paz[l]<<endl;
-                suma=suma+C[i].paz[l];
-            }
-            C[i].vid=suma/k;
+
+            C[i].vid = suma / k;
 
             sort(C[i].paz+0, C[i].paz+k);
 
-            if (k%2==0)
-        {
-        int pirmas=k/2-1;
-        int antras=k/2;
-        C[i].med = (C[i].paz[pirmas] + C[i].paz[antras])/2.0;
+            if (k % 2 == 0) {
+                int pirmas = k / 2 - 1;
+                int antras = k / 2;
+                C[i].med = (C[i].paz[pirmas] + C[i].paz[antras]) / 2.0;
+            } else {
+                int vidurys = k / 2;
+                C[i].med = C[i].paz[vidurys];
+            }
         }
-        else{
-            int vidurys = k / 2;
-            C[i].med = C[i].paz[vidurys];
-            }
-            }
+
         int w;
-        cout<<"Jeigu norite atsitiktinai sugeneruoti egzamino rezultata spauskite 1: "<<endl;
-        cout<<"Jeigu norite ivesti egzamino rezultata spauskite 2: "<<endl;
-        cout<<"Jusu pasirinkimas: "; cin>>w;
-        if(w==1){
-            C[i].egz=(rand()%10)+1;
-            cout<<"Egzamino rezultatas: "<<C[i].egz<<endl;
-        }
-        if(w==2){
-            cout<<"Iveskite egzamino rezultata: ";
-            cin>>C[i].egz;
-            cout<<endl;
+        cout << "Jeigu norite atsitiktinai sugeneruoti egzamino rezultata spauskite 1: " << endl;
+        cout << "Jeigu norite ivesti egzamino rezultata spauskite 2: " << endl;
+        cout << "Jusu pasirinkimas: ";
+        while (!(cin >> w) || (w != 1 && w != 2)) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Iveskite skaiciu 1 arba 2: ";
+    }
+
+
+        if (w == 1) {
+            C[i].egz = (rand() % 10) + 1;
+            cout << "Egzamino rezultatas: " << C[i].egz << endl;
         }
 
-        C[i].galutinis=(C[i].vid*0.4)+(0.6*C[i].egz);
+        if (w == 2) {
+            cout << "Iveskite egzamino rezultata: ";
 
-        cout<<"Noredami baigti studentu generavima spauskite 1, noredami testi spauskite 2 "<<endl;
-        cout<<"Jusu pasirinkimas: "; cin>>qq;
+            while (!(cin >> C[i].egz) || (C[i].egz < 1 || C[i].egz > 10)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Iveskite egzamino rezultata nuo 1 iki 10: ";
+            }
+            cout << endl;
+        }
+
+        C[i].galutinis = (C[i].vid * 0.4) + (0.6 * C[i].egz);
+
+        cout << "Noredami baigti studentu generavima spauskite 1, noredami testi spauskite 2 " << endl;
+        cout << "Jusu pasirinkimas: ";
+
+        while (!(cin >> qq) || (qq != 1 && qq != 2)) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Iveskite skaiciu 1 arba 2: ";
+    }
+
         i++;
-        }
-        cout<<left<<setw(15)<<"Pavarde "<<setw(15)<< "Vardas"<<setw(17)<<"Galutinis (Vid.)"<<" / "<<setw(17)<<"Galutinis (Med.)"<<endl;
+    }
+
+    cout<<left<<setw(15)<<"Pavarde "<<setw(15)<< "Vardas"<<setw(17)<<"Galutinis (Vid.)"<<" / "<<setw(17)<<"Galutinis (Med.)"<<endl;
         cout<<"------------------------------------------------------------"<<endl;
         for(int q=0; q<i; q++)
         {
             cout<<left<<setw(15)<<C[q].pav<<setw(15)<<C[q].var<<setw(17)<<fixed<<setprecision(2)<<C[q].galutinis<<setw(17)<<fixed<<setprecision(2)<<C[q].med<<endl;
 
         }
-        break;
+
+    break;
         }
         case 4:{
         cout<<"Programa baigta."<<endl;
@@ -304,6 +384,7 @@ Pazymiai C[7];
         }
         }
         }
+
 
     return 0;
 }
