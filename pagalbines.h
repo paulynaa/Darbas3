73856b20d9dca76b2ae69f69138b199e778bc849
paulynaa <147087833+paulynaa@users.noreply.github.com>
2024-03-05 19:38:41 +0200

    Add files via upload

---
+++ pagalbines.h
@@ -0,0 +1,31 @@
+#ifndef PAGALBINES_H_INCLUDED
+#define PAGALBINES_H_INCLUDED
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include <stdexcept>
+struct Pazymiai{
+    std::string var;
+    std::string pav;
+    double vid;
+    int egz;
+    std::vector<int> paz;
+    double galutinis;
+    double med;
+};
+
+void rezultatai(std::vector<Pazymiai> hh);
+
+void rezultataifailas(std::vector<Pazymiai> hh, std::string failiukas);
+
+double mediana(int u, Pazymiai h);
+
+void generuojam(int bivi, std::string zhiazhi);
+
+
+#endif // PAGALBINES_H_INCLUDED
