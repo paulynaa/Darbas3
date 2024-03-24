8631499d60422542c2f4f9c85c6d1bbcd26a4a18
paulynaa <147087833+paulynaa@users.noreply.github.com>
2024-03-25 00:26:31 +0200

    Add files via upload

---
+++ pagalbinesvector.h
@@ -0,0 +1,43 @@
+#ifndef PAGALBINESVECTOR_H_INCLUDED
+#define PAGALBINESVECTOR_H_INCLUDED
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include <stdexcept>
+class Pazymiai{
+    private:
+        std::string var;
+        std::string pav;
+        double vid;
+        int egz;
+        std::vector<int> paz;
+        double galutinis;
+        double med;
+    public:
+        Pazymiai(): {};
+};
+
+void rezultatai(std::vector<Pazymiai> hh);
+
+void rezultataifailas(std::vector<Pazymiai> hh, std::string failiukas);
+
+double mediana(int u, Pazymiai h);
+
+void generuojam(int studentusk, std::string failopav);
+
+void failuskaick(std::string wp, Pazymiai hi, std::vector<Pazymiai>& P, std::vector<Pazymiai>& Z);
+
+void failuskaickstrategija1(std::string wp, Pazymiai hi,std::vector<Pazymiai>& S, std::vector<Pazymiai>& P, std::vector<Pazymiai>& Z);
+
+void failuskaickstrategija2(std::string wp, Pazymiai hi, std::vector<Pazymiai>& P, std::vector<Pazymiai>& Z);
+
+void failuskaickstrategija3(std::string wp, Pazymiai hi,std::vector<Pazymiai>& S, std::vector<Pazymiai>& P, std::vector<Pazymiai>& Z);
+
+void spausdintuvas(std::string zekai, std::string malaciai, std::vector<Pazymiai> P, std::vector<Pazymiai> Z );
+
+#endif // PAGALBINES_H_INCLUDED
