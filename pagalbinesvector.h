c0c1197a9807cf3090597c4d164b92afa814f2bc
paulynaa <147087833+paulynaa@users.noreply.github.com>
2024-04-06 14:30:03 +0300

    Add files via upload

--- pagalbinesvector.h
+++ pagalbinesvector.h
@@ -9,8 +9,7 @@
 #include <chrono>
 #include <string>
 #include <stdexcept>
-/*
-class Pazymiai{
+class Pazymiai {
     private:
         std::string var_;
         std::string pav_;
@@ -19,12 +18,56 @@ class Pazymiai{
         std::vector<int> paz_;
         double galutinis_;
         double med_;
+
     public:
         Pazymiai() : vid_(0), egz_(0), galutinis_(0), med_(0) {}
-        Pazymiai(std::string var,std::string pav, double vid, int egz, const std::vector<int>& paz, double galutinis, double med)
+
+        Pazymiai(std::string var, std::string pav, double vid, int egz, const std::vector<int>& paz,
+                 double galutinis, double med)
             : var_(var), pav_(pav), vid_(vid), egz_(egz), paz_(paz), galutinis_(galutinis), med_(med) {}
 
-        ~Pazymiai() {paz_.clear(), var_.clear(), pav_.clear();}
+        ~Pazymiai() {paz_.clear(); var_.clear(), pav_.clear();}
+
+    // Copy constructor
+        Pazymiai(const Pazymiai& other)
+            : var_(other.var_), pav_(other.pav_), vid_(other.vid_), egz_(other.egz_),
+              paz_(other.paz_), galutinis_(other.galutinis_), med_(other.med_) {}
+
+    // Copy assignment operator
+        Pazymiai& operator=(const Pazymiai& other) {
+            if (this != &other) {
+                var_ = other.var_;
+                pav_ = other.pav_;
+                vid_ = other.vid_;
+                egz_ = other.egz_;
+                paz_ = other.paz_;
+                galutinis_ = other.galutinis_;
+                med_ = other.med_;
+            }
+            return *this;
+        }
+
+    // Move constructor
+        Pazymiai(Pazymiai&& other) noexcept
+            : var_(std::move(other.var_)), pav_(std::move(other.pav_)),
+              vid_(other.vid_), egz_(other.egz_), paz_(std::move(other.paz_)),
+              galutinis_(other.galutinis_), med_(other.med_) {}
+
+    // Move assignment operator
+        Pazymiai& operator=(Pazymiai&& other) noexcept {
+            if (this != &other) {
+                var_ = std::move(other.var_);
+                pav_ = std::move(other.pav_);
+                vid_ = other.vid_;
+                egz_ = other.egz_;
+                paz_ = std::move(other.paz_);
+                galutinis_ = other.galutinis_;
+                med_ = other.med_;
+            }
+            return *this;
+        }
+
+
 
         void setVar(const std::string& newVar) { var_ = newVar; }
         void setPav(const std::string& newPav) { pav_ = newPav; }
@@ -34,167 +77,6 @@ class Pazymiai{
         void setPazymiai(const std::vector<int>& pazymiai) { paz_ = pazymiai; }
         void setGalutinis(double newGalutinis) { galutinis_ = newGalutinis; }
         void setMed(double newMed) { med_ = newMed; }
-        void sortPaz(Pazymiai& C) { sort(C.paz_.begin(), C.paz_.end());}
-        std::string getVar() const { return var_; }
-        std::string getPav() const { return pav_; }
-        double getVid() const { return vid_; }
-        int getEgz() const { return egz_; }
-        std::vector<int> getPaz() const { return paz_; }
-        int getPazN(const std::vector<int>& newPaz, int pos) const { return newPaz[pos]; }
-        double getGalutinis() const { return galutinis_; }
-        double getMed() const { return med_; }
-};
-
-class Pazymiai {
-private:
-    std::string var_;
-    std::string pav_;
-    double vid_;
-    int egz_;
-    std::vector<int> paz_;
-    double galutinis_;
-    double med_;
-
-public:
-    // Default constructor
-    Pazymiai() : vid_(0), egz_(0), galutinis_(0), med_(0) {}
-
-    // Constructor with parameters
-    Pazymiai(std::string var, std::string pav, double vid, int egz, const std::vector<int>& paz,
-             double galutinis, double med)
-        : var_(var), pav_(pav), vid_(vid), egz_(egz), paz_(paz), galutinis_(galutinis), med_(med) {}
-
-    // Copy constructor
-    Pazymiai(const Pazymiai& other)
-        : var_(other.var_), pav_(other.pav_), vid_(other.vid_), egz_(other.egz_),
-          paz_(other.paz_), galutinis_(other.galutinis_), med_(other.med_) {}
-
-    // Copy assignment operator
-    Pazymiai& operator=(const Pazymiai& other) {
-        if (this != &other) {
-            var_ = other.var_;
-            pav_ = other.pav_;
-            vid_ = other.vid_;
-            egz_ = other.egz_;
-            paz_ = other.paz_;
-            galutinis_ = other.galutinis_;
-            med_ = other.med_;
-        }
-        return *this;
-    }
-
-    // Move constructor
-    Pazymiai(Pazymiai&& other) noexcept
-        : var_(std::move(other.var_)), pav_(std::move(other.pav_)),
-          vid_(other.vid_), egz_(other.egz_), paz_(std::move(other.paz_)),
-          galutinis_(other.galutinis_), med_(other.med_) {}
-
-    // Move assignment operator
-    Pazymiai& operator=(Pazymiai&& other) noexcept {
-        if (this != &other) {
-            var_ = std::move(other.var_);
-            pav_ = std::move(other.pav_);
-            vid_ = other.vid_;
-            egz_ = other.egz_;
-            paz_ = std::move(other.paz_);
-            galutinis_ = other.galutinis_;
-            med_ = other.med_;
-        }
-        return *this;
-    }
-
-    // Destructor
-    ~Pazymiai() {
-        // No need to clear member variables as they manage their own resources
-    }
-
-    void setVar(const std::string& newVar) { var_ = newVar; }
-        void setPav(const std::string& newPav) { pav_ = newPav; }
-        void setVid(double newVid) { vid_ = newVid; }
-        void setEgz(int newEgz) { egz_ = newEgz; }
-        void setOnePaz(int newPaz) { paz_.push_back(newPaz); }
-        void setPazymiai(const std::vector<int>& pazymiai) { paz_ = pazymiai; }
-        void setGalutinis(double newGalutinis) { galutinis_ = newGalutinis; }
-        void setMed(double newMed) { med_ = newMed; }
-        void sortPaz(Pazymiai& C) { sort(C.paz_.begin(), C.paz_.end());}
-        std::string getVar() const { return var_; }
-        std::string getPav() const { return pav_; }
-        double getVid() const { return vid_; }
-        int getEgz() const { return egz_; }
-        std::vector<int> getPaz() const { return paz_; }
-        int getPazN(const std::vector<int>& newPaz, int pos) const { return newPaz[pos]; }
-        double getGalutinis() const { return galutinis_; }
-        double getMed() const { return med_; }
-};
-*/
-class Pazymiai {
-private:
-    std::string var_;
-    std::string pav_;
-    double vid_;
-    int egz_;
-    std::vector<int> paz_;
-    double galutinis_;
-    double med_;
-
-public:
-    Pazymiai() : vid_(0), egz_(0), galutinis_(0), med_(0) {}
-
-    Pazymiai(std::string var, std::string pav, double vid, int egz, const std::vector<int>& paz,
-             double galutinis, double med)
-        : var_(var), pav_(pav), vid_(vid), egz_(egz), paz_(paz), galutinis_(galutinis), med_(med) {}
-
-    ~Pazymiai() {paz_.clear(); var_.clear(), pav_.clear();}
-
-    // Copy constructor
-    Pazymiai(const Pazymiai& other)
-        : var_(other.var_), pav_(other.pav_), vid_(other.vid_), egz_(other.egz_),
-          paz_(other.paz_), galutinis_(other.galutinis_), med_(other.med_) {}
-
-    // Copy assignment operator
-    Pazymiai& operator=(const Pazymiai& other) {
-        if (this != &other) {
-            var_ = other.var_;
-            pav_ = other.pav_;
-            vid_ = other.vid_;
-            egz_ = other.egz_;
-            paz_ = other.paz_;
-            galutinis_ = other.galutinis_;
-            med_ = other.med_;
-        }
-        return *this;
-    }
-
-    // Move constructor
-    Pazymiai(Pazymiai&& other) noexcept
-        : var_(std::move(other.var_)), pav_(std::move(other.pav_)),
-          vid_(other.vid_), egz_(other.egz_), paz_(std::move(other.paz_)),
-          galutinis_(other.galutinis_), med_(other.med_) {}
-
-    // Move assignment operator
-    Pazymiai& operator=(Pazymiai&& other) noexcept {
-        if (this != &other) {
-            var_ = std::move(other.var_);
-            pav_ = std::move(other.pav_);
-            vid_ = other.vid_;
-            egz_ = other.egz_;
-            paz_ = std::move(other.paz_);
-            galutinis_ = other.galutinis_;
-            med_ = other.med_;
-        }
-        return *this;
-    }
-
-
-
-    void setVar(const std::string& newVar) { var_ = newVar; }
-        void setPav(const std::string& newPav) { pav_ = newPav; }
-        void setVid(double newVid) { vid_ = newVid; }
-        void setEgz(int newEgz) { egz_ = newEgz; }
-        void setOnePaz(int newPaz) { paz_.push_back(newPaz); }
-        void setPazymiai(const std::vector<int>& pazymiai) { paz_ = pazymiai; }
-        void setGalutinis(double newGalutinis) { galutinis_ = newGalutinis; }
-        void setMed(double newMed) { med_ = newMed; }
 
         void sortPaz(Pazymiai& C) { sort(C.paz_.begin(), C.paz_.end());}
 
@@ -208,6 +90,7 @@ public:
         double getMed() const { return med_; }
 
 };
+
 void rezultatai(std::vector<Pazymiai> hh);
 
 void rezultataifailas(std::vector<Pazymiai> hh, std::string failiukas);
