e1e7e7971c3facd1668b2ca1db684200204ff385
paulynaa <147087833+paulynaa@users.noreply.github.com>
2024-04-07 16:07:22 +0300

    Add files via upload

--- pagalbinesvector.h
+++ pagalbinesvector.h
@@ -91,152 +91,99 @@ class Pazymiai {
         double getMed() const { return med_; }
 
 };
-
-class Zmogus {
-protected:
-    std::string var_;
-    std::string pav_;
-public:
-    virtual ~Zmogus() {} // Padarome bazinæ klasæ abstrakèià su virtualiu destruktoriumi
-
-    // Metodai skirti gauti ir nustatyti vardà ir pavardæ
-    virtual void setVar(const std::string& newVar) = 0;
-    virtual void setPav(const std::string& newPav) = 0;
-    virtual std::string getVar() const = 0;
-    virtual std::string getPav() const = 0;
-};
-
-class Pazymiai : public Zmogus {
-private:
-    double vid_;
-    int egz_;
-    std::vector<int> paz_;
-    double galutinis_;
-    double med_;
-public:
-    // Konstruktorius
-    Pazymiai() : vid_(0), egz_(0), galutinis_(0), med_(0) {}
-
-    // Metodai skirti nustatyti paþymiø vidurká, egzamino rezultatà, gauti paþymius ir t.t.
-    void setVid(double newVid) { vid_ = newVid; }
-    void setEgz(int newEgz) { egz_ = newEgz; }
-    void setOnePaz(int newPaz) { paz_.push_back(newPaz); }
-    void setPazymiai(const std::vector<int>& pazymiai) { paz_ = pazymiai; }
-    void setGalutinis(double newGalutinis) { galutinis_ = newGalutinis; }
-    void setMed(double newMed) { med_ = newMed; }
-
-    void sortPaz(Pazymiai& C) { sort(C.paz_.begin(), C.paz_.end());}
-
-    double getVid() const { return vid_; }
-    int getEgz() const { return egz_; }
-    std::vector<int> getPaz() const { return paz_; }
-    int getPazN(const std::vector<int>& newPaz, int pos) const { return newPaz[pos]; }
-    double getGalutinis() const { return galutinis_; }
-    double getMed() const { return med_; }
-
-    // Implementuojame abstrakèiø metodø nustatymà ir gavimà
-    void setVar(const std::string& newVar) override { var_ = newVar; }
-    void setPav(const std::string& newPav) override { pav_ = newPav; }
-    std::string getVar() const override { return var_; }
-    std::string getPav() const override { return pav_; }
-};
 */
 class Zmogus {
-protected:
-    std::string var_;
-    std::string pav_;
+    protected:
+        std::string var_;
+        std::string pav_;
 
-public:
-    Zmogus() = default;
-    Zmogus(const std::string& var, const std::string& pav) : var_(var), pav_(pav) {}
-    virtual ~Zmogus() {}
+    public:
+        Zmogus() = default;
+        Zmogus(const std::string& var, const std::string& pav) : var_(var), pav_(pav) {}
+        virtual ~Zmogus() {}
 
-    virtual void setVar(const std::string& newVar) { var_ = newVar; }
-    virtual void setPav(const std::string& newPav) { pav_ = newPav; }
-    virtual std::string getVar() const { return var_; }
-    virtual std::string getPav() const { return pav_; }
+        virtual void setVar(const std::string& newVar) { var_ = newVar; }
+        virtual void setPav(const std::string& newPav) { pav_ = newPav; }
+        virtual std::string getVar() const { return var_; }
+        virtual std::string getPav() const { return pav_; }
 };
 
 class Pazymiai : public Zmogus {
-private:
-    double vid_;
-    int egz_;
-    std::vector<int> paz_;
-    double galutinis_;
-    double med_;
+    private:
+        double vid_;
+        int egz_;
+        std::vector<int> paz_;
+        double galutinis_;
+        double med_;
 
-public:
-    // Default konstruktorius
-    Pazymiai() : vid_(0), egz_(0), galutinis_(0), med_(0) {}
+    public:
+        Pazymiai() : vid_(0), egz_(0), galutinis_(0), med_(0) {}
 
-    // Konstruktorius su parametrais
-    Pazymiai(const std::string& var, const std::string& pav, double vid, int egz, const std::vector<int>& paz,
-             double galutinis, double med)
-        : Zmogus(var, pav), vid_(vid), egz_(egz), paz_(paz), galutinis_(galutinis), med_(med) {}
+        Pazymiai(const std::string& var, const std::string& pav, double vid, int egz, const std::vector<int>& paz,
+                 double galutinis, double med)
+            : Zmogus(var, pav), vid_(vid), egz_(egz), paz_(paz), galutinis_(galutinis), med_(med) {}
 
-    // Copy konstruktorius
-    Pazymiai(const Pazymiai& other)
-        : Zmogus(other.getVar(), other.getPav()), vid_(other.vid_), egz_(other.egz_),
-          paz_(other.paz_), galutinis_(other.galutinis_), med_(other.med_) {}
+        // copy konstruktorius
+        Pazymiai(const Pazymiai& other)
+            : Zmogus(other.getVar(), other.getPav()), vid_(other.vid_), egz_(other.egz_),
+              paz_(other.paz_), galutinis_(other.galutinis_), med_(other.med_) {}
 
-    // Move konstruktorius
-    Pazymiai(Pazymiai&& other) noexcept
-        : Zmogus(std::move(other.var_), std::move(other.pav_)),
-          vid_(other.vid_), egz_(other.egz_), paz_(std::move(other.paz_)),
-          galutinis_(other.galutinis_), med_(other.med_) {}
+        // move konstruktorius
+        Pazymiai(Pazymiai&& other) noexcept
+            : Zmogus(std::move(other.var_), std::move(other.pav_)),
+              vid_(other.vid_), egz_(other.egz_), paz_(std::move(other.paz_)),
+              galutinis_(other.galutinis_), med_(other.med_) {}
 
-    // Copy priskyrimo operatorius
-    Pazymiai& operator=(const Pazymiai& other) {
-        if (this != &other) {
-            Zmogus::setVar(other.getVar());
-            Zmogus::setPav(other.getPav());
-            vid_ = other.vid_;
-            egz_ = other.egz_;
-            paz_ = other.paz_;
-            galutinis_ = other.galutinis_;
-            med_ = other.med_;
+        // copy assign operatorius
+        Pazymiai& operator=(const Pazymiai& other) {
+            if (this != &other) {
+                Zmogus::setVar(other.getVar());
+                Zmogus::setPav(other.getPav());
+                vid_ = other.vid_;
+                egz_ = other.egz_;
+                paz_ = other.paz_;
+                galutinis_ = other.galutinis_;
+                med_ = other.med_;
+            }
+            return *this;
         }
-        return *this;
-    }
 
-    // Move priskyrimo operatorius
-    Pazymiai& operator=(Pazymiai&& other) noexcept {
-        if (this != &other) {
-            Zmogus::setVar(std::move(other.var_));
-            Zmogus::setPav(std::move(other.pav_));
-            vid_ = other.vid_;
-            egz_ = other.egz_;
-            paz_ = std::move(other.paz_);
-            galutinis_ = other.galutinis_;
-            med_ = other.med_;
+        // move assign operatorius
+        Pazymiai& operator=(Pazymiai&& other) noexcept {
+            if (this != &other) {
+                Zmogus::setVar(std::move(other.var_));
+                Zmogus::setPav(std::move(other.pav_));
+                vid_ = other.vid_;
+                egz_ = other.egz_;
+                paz_ = std::move(other.paz_);
+                galutinis_ = other.galutinis_;
+                med_ = other.med_;
+            }
+            return *this;
         }
-        return *this;
-    }
 
-    // Destructor
-    ~Pazymiai() {}
+        ~Pazymiai() {paz_.clear();}
 
-    // Metodai skirti nustatyti paþymiø vidurká, egzamino rezultatà, gauti paþymius ir t.t.
-    void setVid(double newVid) { vid_ = newVid; }
-    void setEgz(int newEgz) { egz_ = newEgz; }
-    void setOnePaz(int newPaz) { paz_.push_back(newPaz); }
-    void setPazymiai(const std::vector<int>& pazymiai) { paz_ = pazymiai; }
-    void setGalutinis(double newGalutinis) { galutinis_ = newGalutinis; }
-    void setMed(double newMed) { med_ = newMed; }
+        void setVid(double newVid) { vid_ = newVid; }
+        void setEgz(int newEgz) { egz_ = newEgz; }
+        void setOnePaz(int newPaz) { paz_.push_back(newPaz); }
+        void setPazymiai(const std::vector<int>& pazymiai) { paz_ = pazymiai; }
+        void setGalutinis(double newGalutinis) { galutinis_ = newGalutinis; }
+        void setMed(double newMed) { med_ = newMed; }
 
-    void sortPaz(Pazymiai& C) { std::sort(C.paz_.begin(), C.paz_.end()); }
+        void sortPaz(Pazymiai& C) { std::sort(C.paz_.begin(), C.paz_.end()); }
 
-    double getVid() const { return vid_; }
-    int getEgz() const { return egz_; }
-    std::vector<int> getPaz() const { return paz_; }
-    int getPazN(const std::vector<int>& newPaz, int pos) const { return newPaz[pos]; }
-    double getGalutinis() const { return galutinis_; }
-    double getMed() const { return med_; }
+        double getVid() const { return vid_; }
+        int getEgz() const { return egz_; }
+        std::vector<int> getPaz() const { return paz_; }
+        int getPazN(const std::vector<int>& newPaz, int pos) const { return newPaz[pos]; }
+        double getGalutinis() const { return galutinis_; }
+        double getMed() const { return med_; }
 
-    void setVar(const std::string& newVar) override { Zmogus::setVar(newVar); }
-    void setPav(const std::string& newPav) override { Zmogus::setPav(newPav); }
-    std::string getVar() const override { return Zmogus::getVar(); }
-    std::string getPav() const override { return Zmogus::getPav(); }
+        void setVar(const std::string& newVar) override { Zmogus::setVar(newVar); }
+        void setPav(const std::string& newPav) override { Zmogus::setPav(newPav); }
+        std::string getVar() const override { return Zmogus::getVar(); }
+        std::string getPav() const override { return Zmogus::getPav(); }
 };
 
 void rezultatai(std::vector<Pazymiai> hh);
